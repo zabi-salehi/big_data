f6ade3be5e1232c45cc634e003f3bb917d8d7e7f
Florian Frey <florianfrey@gmx.de>
2022-08-17 15:41:27 +0200

    Update index.js
    - adjust web-app to our project
    - review "@?" later

--- web-app/index.js
+++ web-app/index.js
@@ -7,8 +7,8 @@ const MemcachePlus = require('memcache-plus')
 const express = require('express')
 
 const app = express()
-const cacheTimeSecs = 15
-const numberOfMissions = 30
+const cacheTimeSecs = 30
+const numberOfTitles = 5332
 
 // -------------------------------------------------------
 // Command-line options
@@ -29,7 +29,7 @@ let options = optionparser
 	// Database options
 	.option('--mariadb-host <host>', 'MariaDB host', 'my-app-mariadb-service')
 	.option('--mariadb-port <port>', 'MariaDB port', 3306)
-	.option('--mariadb-schema <db>', 'MariaDB Schema/database', 'popular')
+	.option('--mariadb-schema <db>', 'MariaDB Schema/database', 'popular') // @?
 	.option('--mariadb-username <username>', 'MariaDB username', 'root')
 	.option('--mariadb-password <password>', 'MariaDB password', 'mysecretpw')
 	// Misc
@@ -153,24 +153,24 @@ function sendResponse(res, html, cachedResult) {
 		<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Big Data Use-Case Demo</title>
+			<title>Big Data Netflix Ratings</title>
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
 			<script>
-				function fetchRandomMissions() {
-					const maxRepetitions = Math.floor(Math.random() * 200)
-					document.getElementById("out").innerText = "Fetching " + maxRepetitions + " random missions, see console output"
+				function fetchRandomTitles() {
+					const maxRepetitions = Math.floor(Math.random() * 250)
+					document.getElementById("out").innerText = "Fetching " + maxRepetitions + " random titles, see console output"
 					for(var i = 0; i < maxRepetitions; ++i) {
-						const missionId = Math.floor(Math.random() * ${numberOfMissions})
-						console.log("Fetching mission id " + missionId)
-						fetch("/missions/sts-" + missionId, {cache: 'no-cache'})
+						const title = Math.floor(Math.random() * ${numberOfTitles})
+						console.log("Fetching title " + title)
+						fetch("/library/" + title, {cache: 'no-cache'})
 					}
 				}
 			</script>
 		</head>
 		<body>
-			<h1>Big Data Use Case Demo</h1>
+			<h1>Welcome to Netflix</h1>
 			<p>
-				<a href="javascript: fetchRandomMissions();">Randomly fetch some missions</a>
+				<a href="javascript: fetchRandomTitles();">Randomly simulate some views</a>
 				<span id="out"></span>
 			</p>
 			${html}
@@ -191,9 +191,9 @@ function sendResponse(res, html, cachedResult) {
 // Start page
 // -------------------------------------------------------
 
-// Get list of missions (from cache or db)
-async function getMissions() {
-	const key = 'missions'
+// Get list of titles for library (from cache or db)
+async function getTitles() {
+	const key = 'titles'
 	let cachedata = await getFromCache(key)
 
 	if (cachedata) {
@@ -201,7 +201,7 @@ async function getMissions() {
 		return { result: cachedata, cached: true }
 	} else {
 		console.log(`Cache miss for key=${key}, querying database`)
-		const data = await executeQuery("SELECT mission FROM missions", [])
+		const data = await executeQuery("SELECT title FROM netflix_titles", [])
 		if (data) {
 			let result = data.map(row => row?.[0])
 			console.log("Got result=", result, "storing in cache")
@@ -209,52 +209,52 @@ async function getMissions() {
 				await memcached.set(key, result, cacheTimeSecs);
 			return { result, cached: false }
 		} else {
-			throw "No missions data found"
+			throw "No title data found"
 		}
 	}
 }
 
-// Get popular missions (from db only)
+// Get titles with best rating (from db only)
 async function getPopular(maxCount) {
-	const query = "SELECT mission, count FROM popular ORDER BY count DESC LIMIT ?"
+	const query = "SELECT title, rating FROM rating ORDER BY rating DESC LIMIT ?"
 	return (await executeQuery(query, [maxCount]))
-		.map(row => ({ mission: row?.[0], count: row?.[1] }))
+		.map(row => ({ title: row?.[0], rating: row?.[1] })) //@?
 }
 
 // Return HTML for start page
 app.get("/", (req, res) => {
-	const topX = 10;
-	Promise.all([getMissions(), getPopular(topX)]).then(values => {
-		const missions = values[0]
+	const topX = 15;
+	Promise.all([getTitles(), getPopular(topX)]).then(values => {
+		const library = values[0]
 		const popular = values[1]
 
-		const missionsHtml = missions.result
-			.map(m => `<a href='missions/${m}'>${m}</a>`)
+		const libraryHTML = library.result
+			.map(m => `<a href='library/${m}'>${m}</a>`)
 			.join(", ")
 
-		const popularHtml = popular
-			.map(pop => `<li> <a href='missions/${pop.mission}'>${pop.mission}</a> (${pop.count} views) </li>`)
+		const popularHTML = popular
+			.map(pop => `<li> <a href='library/${pop.title}'>${pop.title}</a> (${pop.rating} rating) </li>`)
 			.join("\n")
 
 		const html = `
-			<h1>Top ${topX} Missions</h1>		
+			<h1>Popular Shows and Movies:</h1>
 			<p>
-				<ol style="margin-left: 2em;"> ${popularHtml} </ol> 
+				<ol style="margin-left: 2em;"> ${popularHTML} </ol> 
 			</p>
-			<h1>All Missions</h1>
-			<p> ${missionsHtml} </p>
+			<h1>Full Library:</h1>
+			<p> ${libraryHTML} </p>
 		`
-		sendResponse(res, html, missions.cached)
+		sendResponse(res, html, library.cached)
 	})
 })
 
 // -------------------------------------------------------
-// Get a specific mission (from cache or DB)
+// Get a specific title (from cache or DB)
 // -------------------------------------------------------
 
-async function getMission(mission) {
-	const query = "SELECT mission, heading, description FROM missions WHERE mission = ?"
-	const key = 'mission_' + mission
+async function getTitle(title) {
+	const query = "SELECT * FROM netflix_titles WHERE title = ?"
+	const key = 'title_' + title //@?
 	let cachedata = await getFromCache(key)
 
 	if (cachedata) {
@@ -263,33 +263,35 @@ async function getMission(mission) {
 	} else {
 		console.log(`Cache miss for key=${key}, querying database`)
 
-		let data = (await executeQuery(query, [mission]))?.[0] // first entry
+		let data = (await executeQuery(query, [title]))?.[0] // first entry
 		if (data) {
-			let result = { mission: data?.[0], heading: data?.[1], description: data?.[2] }
+			let result = { title: data?.[0], director: data?.[1], cast: data?.[2], country: data?.[3], 
+				release_year: data?.[4], duration: data?.[5], genre: data?.[6], description: data?.[7] }
 			console.log(`Got result=${result}, storing in cache`)
 			if (memcached)
 				await memcached.set(key, result, cacheTimeSecs);
 			return { ...result, cached: false }
 		} else {
-			throw "No data found for this mission"
+			throw "No data found for this title"
 		}
 	}
 }
 
-app.get("/missions/:mission", (req, res) => {
-	let mission = req.params["mission"]
+app.get("/library/:title", (req, res) => {
+	let title = req.params["title"]
 
 	// Send the tracking message to Kafka
 	sendTrackingMessage({
-		mission,
+		title,
 		timestamp: Math.floor(new Date() / 1000)
-	}).then(() => console.log(`Sent mission=${mission} to kafka topic=${options.kafkaTopicTracking}`))
+	}).then(() => console.log(`Sent title=${title} to kafka topic=${options.kafkaTopicTracking}`))
 		.catch(e => console.log("Error sending to kafka", e))
 
 	// Send reply to browser
-	getMission(mission).then(data => {
-		sendResponse(res, `<h1>${data.mission}</h1><p>${data.heading}</p>` +
-			data.description.split("\n").map(p => `<p>${p}</p>`).join("\n"),
+	getTitle(title).then(data => {
+		sendResponse(res, `<h1>${data.title}(${data.release_year})</h1><p>${data.description}</p><b>${data.duration}</b> ` +
+		`<p>Director: ${data.director}</p><p>Actors: ${data.cast}</p><p>Country: ${data.country}</p><p>Tags: ${data.genre}</p> `,
+			// data.description.split("\n").map(p => `<p>${p}</p>`).join("\n"),
 			data.cached
 		)
 	}).catch(err => {
